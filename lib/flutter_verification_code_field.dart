59513baa27ec6973bb37290a53a85ff39ef4524f
Eugene Martseniuk <eugene.martseniuk@otakoyi.com>
2023-09-07 13:37:44 +0300

    fix: revert

--- lib/flutter_verification_code_field.dart
+++ lib/flutter_verification_code_field.dart
@@ -1,3 +1,1 @@
-export 'src/hooks/focus_node_list_hook.dart' hide useFocusNodeList;
-export 'src/hooks/text_controller_list_hook.dart' hide useTextControllerList;
 export 'src/widgets/verification_code_field.dart' show VerificationCodeField;
