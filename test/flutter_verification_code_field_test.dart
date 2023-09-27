3a9bb4a1bd5a33d2aceddfa7245c821768c9ea9a
yaroslav-siomka <yaroslav.siomka@honeycombsoft.com>
2023-09-27 22:52:05 +0300

    feat: regexp tests

--- test/flutter_verification_code_field_test.dart
+++ test/flutter_verification_code_field_test.dart
@@ -7,34 +7,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'test_utils.dart';
 
 void main() {
-  setUp(() async {
-    final random = Random();
-    String clipboardContent = '';
-
-    for (var i = 0; i < 6; i++) {
-      clipboardContent = clipboardContent + random.nextInt(9).toString();
-    }
-
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      SystemChannels.platform,
-      (methodCall) async {
-        if (methodCall.method == 'Clipboard.getData') {
-          return <String, dynamic>{'text': clipboardContent};
-        }
-        return null;
-      },
-    );
-  });
-
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      SystemChannels.platform,
-      null,
-    );
-  });
-
   testWidgets('Check the number of fields and nodes', (tester) async {
     await tester.prepare();
     expect(finder, findsNWidgets(length));
@@ -69,40 +41,152 @@ void main() {
     expect(nodes.first.hasFocus, true);
   });
 
-  testWidgets('Text is pasted', (tester) async {
-    await tester.prepare();
-    final nodes = tester.getFocusNodes();
-    final textFields = tester.getTextFields();
+  group('description', () {
+    setupPaste();
 
-    final text = await Clipboard.getData(Clipboard.kTextPlain);
-    if (!nodes.first.hasFocus) {
-      nodes.first.requestFocus();
-    }
+    testWidgets('Text is pasted', (tester) async {
+      await tester.prepare();
+      final nodes = tester.getFocusNodes();
+      final textFields = tester.getTextFields();
 
-    final repeatTextPaste = RepeatTextPaste();
+      final text = await Clipboard.getData(Clipboard.kTextPlain);
+      if (!nodes.first.hasFocus) {
+        nodes.first.requestFocus();
+      }
 
-    if (text case final clipboard?) {
-      if (clipboard.text case final pastedText?) {
-        for (int i = 0; i < textFields.length; i++) {
-          await repeatTextPaste.repeatWithParameters(
-            tester: tester,
-            text: pastedText,
-            startingPoint: i,
-            textFields: textFields,
-            node: nodes[i],
-          );
-        }
-        for (int i = 0; i < textFields.length; i++) {
-          await repeatTextPaste.repeatWithParameters(
-            isLogicalKeyboard: true,
-            tester: tester,
-            text: pastedText,
-            startingPoint: i,
-            textFields: textFields,
-            node: nodes[i],
-          );
+      final repeatTextPaste = RepeatTextPaste();
+
+      if (text case final clipboard?) {
+        if (clipboard.text case final pastedText?) {
+          for (int i = 0; i < textFields.length; i++) {
+            await repeatTextPaste.repeatWithParameters(
+              tester: tester,
+              text: pastedText,
+              startingPoint: i,
+              textFields: textFields,
+              node: nodes[i],
+            );
+          }
+          for (int i = 0; i < textFields.length; i++) {
+            await repeatTextPaste.repeatWithParameters(
+              isLogicalKeyboard: true,
+              tester: tester,
+              text: pastedText,
+              startingPoint: i,
+              textFields: textFields,
+              node: nodes[i],
+            );
+          }
         }
       }
+    });
+    tearDown(() {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        SystemChannels.platform,
+        null,
+      );
+    });
+  });
+  group('Group with different regexp', () {
+    setupPaste(isRegExpChanged: true);
+
+    testWidgets('New RegExp', (tester) async {
+      await tester.prepare(RegExp(r'[a-zA-Z0-9]'));
+
+      List<FocusNode> nodes = tester.getFocusNodes();
+      List<TextField> textFields = tester.getTextFields();
+
+      final text = await Clipboard.getData(Clipboard.kTextPlain);
+      if (!nodes.first.hasFocus) {
+        nodes.first.requestFocus();
+      }
+
+      final repeatTextPaste = RepeatTextPaste();
+      if (text case final clipboard?) {
+        if (clipboard.text case final pastedText?) {
+          for (int i = 0; i < textFields.length; i++) {
+            await repeatTextPaste.repeatWithParameters(
+              tester: tester,
+              text: pastedText,
+              startingPoint: i,
+              textFields: textFields,
+              node: nodes[i],
+            );
+          }
+        }
+      }
+    });
+
+    testWidgets('Default RegExp', (tester) async {
+      await tester.prepare();
+
+      final nodes = tester.getFocusNodes();
+      final textFields = tester.getTextFields();
+
+      if (!nodes.first.hasFocus) {
+        nodes.first.requestFocus();
+      }
+      for (int i = 0; i < textFields.length; i++) {
+        if (!nodes[i].hasFocus) {
+          nodes[i].requestFocus();
+          await tester.pump(const Duration(milliseconds: 100));
+        }
+
+        await tester.longPress(find.byWidget(textFields[0]));
+        await tester.pump(const Duration(milliseconds: 100));
+        await tester.tap(find.text('Paste'));
+        await tester.pump(const Duration(milliseconds: 100));
+
+        for (int i = 0; i < textFields.length; i++) {
+          expect(textFields[i].controller!.text, '');
+        }
+      }
+    });
+
+    tearDown(() {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        SystemChannels.platform,
+        null,
+      );
+    });
+  });
+}
+
+void setupPaste({bool isRegExpChanged = false}) {
+  setUp(() async {
+    final random = Random();
+    String clipboardContent = '';
+
+    if (isRegExpChanged) {
+      const chars =
+          'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+
+      String getRandomString(int length) => String.fromCharCodes(
+            Iterable.generate(
+              length,
+              (_) => chars.codeUnitAt(
+                random.nextInt(chars.length),
+              ),
+            ),
+          );
+      clipboardContent = '${getRandomString(4)}12';
+    } else {
+      for (var i = 0; i < 6; i++) {
+        clipboardContent = clipboardContent + random.nextInt(9).toString();
+      }
     }
+
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      SystemChannels.platform,
+      (methodCall) async {
+        if (methodCall.method == 'Clipboard.getData') {
+          return <String, dynamic>{'text': clipboardContent};
+        }
+        return null;
+      },
+    );
   });
 }
